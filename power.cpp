cb1c41b248a617cdb57752f4e66c6a884d66a2ee
Arin Zingade <arinzingade29@gmail.com>
2024-02-09 23:01:09 +0530

    Template MetaProgramming

--- power.cpp
+++ power.cpp
@@ -1,19 +1,20 @@
+
 #include <iostream>
 #include <array>
 #include <chrono>
 using namespace std;
 
-// Regular runtime initialization
+
 template <size_t N>
-std::array<int, N> initialize_array_runtime() {
-    std::array<int, N> arr;
+array<int, N> initialize_array_runtime() {
+    array<int, N> arr;
     for (size_t i = 0; i < N; ++i) {
         arr[i] = i;
     }
     return arr;
 }
 
-// Template metaprogramming for compile-time initialization
+
 template <size_t... Indices>
 constexpr std::array<int, sizeof...(Indices)> initialize_array_metaprogramming_impl(std::index_sequence<Indices...>) {
     return {Indices...};
@@ -33,14 +34,14 @@ int main() {
 
     auto dur1 = chrono::duration_cast<chrono::nanoseconds>(end1 - start1);
 
-    // Compile-time initialization using template metaprogramming
+    
     auto start2 = chrono::high_resolution_clock::now();
     constexpr auto arr_metaprogramming = initialize_array_metaprogramming<array_size>();
     auto end2 = chrono::high_resolution_clock::now();
 
     auto dur2 = chrono::duration_cast<chrono::nanoseconds>(end2 - start2);
 
-    // Output the results
+    
     std::cout << "Runtime Initialization: ";
     for (const auto& elem : arr_runtime) {
         std::cout << elem << " ";
@@ -49,6 +50,7 @@ int main() {
     cout << "Time taken for Normal Execution: " << dur1.count() << endl;
     cout << "Time taken for metaprogramming: " << dur2.count() << endl;
 
+    cout << "Hence, Template Programming execution is always constant time " << endl;
 
     return 0;
 }
